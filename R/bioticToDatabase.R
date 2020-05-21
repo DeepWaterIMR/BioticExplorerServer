af7b4e3b40e3273820c43a8c7ddb74ccac2f38a3
MikkoVihtakari <mikko.vihtakari@hi.no>
2020-05-21 09:40:34 +0200

    Fixed a bug in bioticToDatabase

--- R/bioticToDatabase.R
+++ R/bioticToDatabase.R
@@ -1,118 +1,130 @@
 #' @title Read and process a NMD Biotic xml file for further use in the BioticExplorer database
 #' @description A wrapper for \code{\link[RstoxData]{readXmlFile}} to enable further use in the BioticExplorer database
 #' @param file character string specifying the file path to the xml file. Accepts only one file at the time.
-#' @param removeEmpty logical indicating whether empty columns should be removed from the output. This option also influences "coreData" columns.
+#' @param removeEmpty logical indicating whether empty columns should be removed from the output. 
 #' @param convertColumns logical indicating whether the column types should be converted. See \code{link{convertColumnTypes}}. Setting this to \code{FALSE} considerably speeds up the function, but leads to problems with non-unicode characters.
-#' @param missionidPrefix A prefix for the \code{missionid} identifier, which separates cruises. \code{NULL} (default) omits the prefix. Use year when writing to the database.
-#' @return Returns a list of Biotic data with the \code{$mission} data table from the original NMD data. The \code{$stnall} and \code{$indall} data frames are merged from \code{$fishstation} and \code{$catchsample} (former) and  \code{$fishstation}, \code{$catchsample}, \code{$individual} and \code{$agedetermination} (latter).
-#' @author Mikko Vihtakari, Ibrahim Umar (Institute of Marine Research)
+#' @param returnOriginal logical indicating whether the original data (\code{$mission} through \code{$agedetermination}) should be returned together with combined data.
+#' @param missionidPrefix A prefix for the \code{missionid} identifier, which separates cruises. Used in \code{\link{processBioticFiles}} function when several xml files are put together. \code{NULL} (default) omits the prefix. Not needed in \code{processBioticFile} function.
+#' @details This function should be identical to the BioticExplorer::processBioticFile function. 
+#' @return Returns a list of Biotic data with \code{$mission}, \code{$stnall} and \code{$indall} data tables. The \code{$stnall} and \code{$indall} are merged from \code{$fishstation} and \code{$catchsample} (former) and  \code{$fishstation}, \code{$catchsample}, \code{$individual} and \code{$agedetermination} (latter). 
+#' @author Mikko Vihtakari, Ibrahim Umar (Institute of Marine Research) 
 #' @import RstoxData data.table
 #' @export
 
+
 # Debugging parameters
-bioticToDatabase <- function(file, removeEmpty = FALSE, convertColumns = TRUE, missionidPrefix = NULL) {
-
+# removeEmpty = TRUE; convertColumns = TRUE; returnOriginal = FALSE; missionidPrefix = NULL
+bioticToDatabase <- function(file, removeEmpty = TRUE, convertColumns = TRUE, returnOriginal = FALSE, missionidPrefix = NULL) {
+  
   ## Checks
-
+  
   if(!file.exists(file)) stop("file does not exist. Check your file path.")
-
+  
   ## Read the Biotic file ----
-
+  
   dt <- RstoxData::readXmlFile(file)
-
+  
   ## Mission data ---
-
+  
   msn <- dt$mission
-
+  
   if (convertColumns) {
     date.cols <- grep("date", names(msn), value = TRUE)
     msn[, eval(date.cols) := lapply(.SD, as.Date), .SDcols = eval(date.cols)]
   }
-
+  
   if (is.null(missionidPrefix)) {
     msn$missionid <- rownames(msn)
   } else {
     msn$missionid <- paste(missionidPrefix, rownames(msn), sep = "_")
   }
-
+  
   ## Station data ---
-
+  
   stn <- dt$fishstation
-
+  
   stn[is.na(stationstarttime), stationstarttime := "00:00:00.000Z"]
-
+  stn[is.na(stationstoptime), stationstoptime := "00:00:00.000Z"]
+  
   stn[, stationstartdate := as.POSIXct(paste(stn$stationstartdate, stn$stationstarttime), format = "%Y-%m-%dZ %H:%M:%S", tz = "GMT")]
-
+  stn[, stationstopdate := as.POSIXct(paste(stn$stationstopdate, stn$stationstoptime), format = "%Y-%m-%dZ %H:%M:%S", tz = "GMT")]
+  
   stn[, stationstarttime := NULL]
-
+  stn[, stationstoptime := NULL]
+  
   if (convertColumns) {
     date.cols <- grep("date", names(stn), value = TRUE)
     stn[, eval(date.cols) := lapply(.SD, as.Date), .SDcols = eval(date.cols)]
   }
-
+  
   ##________________
   ## Sample data ---
-
+  
   cth <- dt$catchsample
-
+  
   ##____________________
   ## Individual data ---
-
+  
   ind <- dt$individual
-
+  
   ## Age data ---
-
+  
   age <- dt$agedetermination
-
+  
   if (convertColumns) {
     date.cols <- grep("date", names(age), value = TRUE)
     age[, eval(date.cols) := lapply(.SD, as.Date), .SDcols = eval(date.cols)]
   }
-
+  
   # if (nrow(age) == 0) {
   #   age <- rapply(age, as.integer, how = "replace")
   # }
-
+  
   ## Compiled datasets ----
-
-  coredat <- merge(msn[,!names(msn) %in% c("purpose")], stn, by = names(msn)[names(msn) %in% names(stn)], all = TRUE)
-
+  
+  coredat <- merge(msn[,!names(msn) %in% c("purpose"), with = FALSE], stn, by = names(msn)[names(msn) %in% names(stn)], all = TRUE)
+  
   # Stndat
-
+  
   stndat <- merge(coredat, cth, all.y = TRUE, by = c("missiontype", "missionnumber", "startyear", "platform", "serialnumber"))
-
+  
   # Inddat
-
-  inddat <- merge(stndat[,!names(stndat) %in% c("purpose", "stationcomment"), with = FALSE], ind, all.y = TRUE, by = names(stndat)[names(stndat) %in% names(ind)])
-
+  
+  inddat <- merge(stndat[,!names(stndat) %in% c("purpose", "stationcomment", "catchcomment"), with = FALSE], ind, all.y = TRUE, by = names(stndat)[names(stndat) %in% names(ind)])
+  
   inddat[is.na(preferredagereading), preferredagereading := 1]
   inddat <- merge(inddat, age, by.x=c(intersect(names(inddat), names(age)), "preferredagereading"), by.y= c(intersect(names(inddat), names(age)), "agedeterminationid"), all.x = TRUE)
-
-  # inddat[is.na(inddat$commonname), "commonname"] <- "Merging error due to missing data"
-
+  
+  if(sum(is.na(inddat$commonname)) > 0) stop(paste(sum(is.na(inddat$commonname)), "missing commonname records. This is likely due to merging error between individual and agedetermination data tables. File a bug report."))
+  
   ## Return ----
-
+  
   ### Format
-
-  out <- list(mission = msn, stnall = stndat, indall = inddat)
-
+  
+  if (returnOriginal) {
+    out <- list(mission = msn, fishstation = stn, catchsample = cth, individual = ind, agedetermination = age, stnall = stndat, indall = inddat)
+  } else {
+    out <- list(mission = msn, stnall = stndat, indall = inddat)
+  }
+  
   ### Remove empty columns to save space
-
+  
   if (removeEmpty) {
     out <- lapply(out, function(k) {
       k[, which(unlist(lapply(k, function(x) !all(is.na(x))))), with = FALSE]
     })
   }
-
+  
   ### Class
-
+  
   class(out) <- "bioticProcData"
-
+  
   ### Return
-
+  
   out
-
+  
 }
+
 
 #' @title Print processed NMD Biotic data (\code{bioticProcData}) objects
 #' @description \code{\link{print}} function for \code{\link[=bioticToDatabase]{bioticProcData}} objects
